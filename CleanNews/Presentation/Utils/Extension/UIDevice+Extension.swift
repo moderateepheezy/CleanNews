21e3788680458bb824514f42e70274abdccfdb56
moderateepheezy <moderateepheezy@gmail.com>
2022-01-04 03:29:47 +0100

    Fix swiftlint trailing-comma

--- CleanNews/Presentation/Utils/Extension/UIDevice+Extension.swift
+++ CleanNews/Presentation/Utils/Extension/UIDevice+Extension.swift
@@ -10,15 +10,33 @@ import UIKit
 
 extension UIDevice {
     static var isSmallScreen: Bool {
-        [Hardware.iphone_se, .iphone_se_2g, .ipod_touch_1g,
-         .ipod_touch_2g, .ipod_touch_3g, .ipod_touch_4g,
-         .ipod_touch_5g, .ipod_touch_6g, .ipod_touch_7g,
-         .iphone_12_mini, .iphone_13_mini, .iphone_6,
-         .iphone_6_plus, .iphone_6s_plus, .iphone_6s,
-         .iphone_7, .iphone_7_gsm, .iphone_7_plus,
-         .iphone_7_plus_gsm, .iphone_8, .iphone_8_cn,
-         .iphone_8_plus, .iphone_8_plus_cn,
-         .iphone_8_plus_cn]
-            .contains(DeviceGuru().hardware())
+        let devices = [
+            Hardware.iphone_se,
+            .iphone_se_2g,
+            .ipod_touch_1g,
+            .ipod_touch_2g,
+            .ipod_touch_3g,
+            .ipod_touch_4g,
+            .ipod_touch_5g,
+            .ipod_touch_6g,
+            .ipod_touch_7g,
+            .iphone_12_mini,
+            .iphone_13_mini,
+            .iphone_6,
+            .iphone_6_plus,
+            .iphone_6s_plus,
+            .iphone_6s,
+            .iphone_7,
+            .iphone_7_gsm,
+            .iphone_7_plus,
+            .iphone_7_plus_gsm,
+            .iphone_8,
+            .iphone_8_cn,
+            .iphone_8_plus,
+            .iphone_8_plus_cn,
+            .iphone_8_plus_cn,
+        ]
+
+        return devices.contains(DeviceGuru().hardware())
     }
 }
